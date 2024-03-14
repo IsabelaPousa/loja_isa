1766ae62ac95852d395a39dd2b3a2d9fb7cffcd0
IsabelaPousa <163446180+IsabelaPousa@users.noreply.github.com>
2024-03-14 15:23:23 -0300

    Add files via upload
    Commit realizado com alterações aplicadas na aula do dia 14 de março.

---
+++ footer.php
@@ -0,0 +1,9 @@
+</div>
+
+<!-- Container que abrigará o rodapé da página -->
+<div class="container-fluid text-center bg-dark text-light fixed-bottom">
+    <p>Sistema Desenvolvido por Isabela</p>
+</div>
+
+</body>
+</html>
